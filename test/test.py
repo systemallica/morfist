747af9811b7a65c8a0190b829e972c6c9abb8680
Andrés Reverón Molina <andressser@gmail.com>
2020-02-13 17:35:29 +0100

    feat: reworked tests

--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_boston, load_breast_cancer
 import numpy as np
+from time import perf_counter
 
 # Configuration
 # Number of tress of the random forest
@@ -20,14 +21,13 @@ x_classification, y_classification = load_breast_cancer(return_X_y=True)
 # Test morfist against scikit-learn for a classification task
 def test_class():
     # Fit morfist classification tree
+    t1_start = perf_counter()
+
     cls_morfist = MixedRandomForest(
         n_estimators=n_trees,
         min_samples_leaf=1,
         class_targets=[0]
     )
-
-    # Fit scikit classification tree
-    cls_scikit = RandomForestClassifier(n_estimators=n_trees)
 
     # Calculate morfist scores using cross-validation
     morfist_scores = cross_validation(
@@ -37,13 +37,23 @@ def test_class():
         class_targets=[0],
         folds=n_folds
     )
+    t1_stop = perf_counter()
+    print("Elapsed time morfist cls:", t1_stop - t1_start)
+
+    t2_start = perf_counter()
+    # Fit scikit classification tree
+    cls_scikit = RandomForestClassifier(n_estimators=n_trees)
 
     # Calculate scikit scores using cross-validation
     scikit_scores = cross_val_score(
         cls_scikit,
         x_classification,
-        y_classification
+        y_classification,
+        cv=n_folds
     )
+
+    t2_stop = perf_counter()
+    print("Elapsed time scikit cls:", t2_stop - t2_start)
 
     print('Classification: ')
     print('\tmorfist (accuracy): {}'.format(morfist_scores.mean()))
@@ -53,13 +63,11 @@ def test_class():
 # Test morfist against scikit-learn for a regression task
 def test_reg():
     # Fit morfist regression tree
+    t1_start = perf_counter()
     reg_morfist = MixedRandomForest(
         n_estimators=n_trees,
         min_samples_leaf=5
     )
-
-    # Fit scikit regression tree
-    reg_scikit = RandomForestRegressor(n_estimators=n_trees)
 
     # Calculate morfist scores using cross-validation
     scores_morfist = cross_validation(
@@ -68,14 +76,25 @@ def test_reg():
         y_regression,
         folds=n_folds
     )
+    t1_stop = perf_counter()
+    print("Elapsed time morfist reg:", t1_stop - t1_start)
+
+    t2_start = perf_counter()
+
+    # Fit scikit regression tree
+    reg_scikit = RandomForestRegressor(n_estimators=n_trees)
 
     # Calculate scikit scores using cross-validation
     scores_scikit = cross_val_score(
         reg_scikit,
         x_regression,
         y_regression,
+        cv=n_folds,
         scoring='neg_mean_squared_error'
     )
+
+    t2_stop = perf_counter()
+    print("Elapsed time scikit reg:", t2_stop - t2_start)
 
     print('Regression: ')
     print('\tmorfist (rmse): {}'.format(scores_morfist.mean()))
@@ -86,6 +105,7 @@ def test_reg():
 def test_mix_1():
     # Create data with data transposed and stacked vertically, X is regression, Y is classification
     x_mix_1, y_mix_1 = x_regression, np.vstack([y_regression, y_regression < y_regression.mean()]).T
+    t1_start = perf_counter()
 
     mix_rf = MixedRandomForest(
         n_estimators=n_trees,
@@ -100,6 +120,8 @@ def test_mix_1():
         folds=n_folds,
         class_targets=[1]
     )
+    t1_stop = perf_counter()
+    print("Elapsed time morfist mix:", t1_stop - t1_start)
     print('Mixed output: ')
     print('\ttask 1 (original) (rmse): {}'.format(mix_scores[0]))
     print('\ttask 2 (additional) (accuracy): {}'.format(mix_scores[1]))

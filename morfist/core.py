fbac49f519632c72cdc18f6a36085a09b2f252a7
Andrés Reverón Molina <andressser@gmail.com>
2020-02-14 18:02:16 +0100

    chore: more documentation

--- morfist/core.py
+++ morfist/core.py
@@ -21,6 +21,8 @@ class MixedSplitter:
         self.choose_split = choose_split
 
     def split(self, x, y):
+        # Maximum number of features to try for the best split
+        # Not all of them are tried because this is a Random Tree
         if self.max_features == 'sqrt':
             self.max_features = int(np.ceil(np.sqrt(self.n_features)))
 
@@ -37,21 +39,26 @@ class MixedSplitter:
         # Best impurity
         best_imp = -np.inf
 
+        # Random selection of the features to try for the best split
         try_features = np.random.choice(
             np.arange(self.n_features),
             self.max_features,
             replace=False
         )
 
+        # Try each of the selected features and find which of them gives the best split(higher impurity)
         for f in try_features:
             values = np.unique(x[:, f])
+            # We ensure that the value appears at least 1 times (FIXME??)
             if values.size < 2:
                 continue
-
+            # TODO: what's going on here
             values = (values[:-1] + values[1:]) / 2
 
             # random value sub-sampling
             values = np.random.choice(values, min(2, values.size))
+            # Try to split with this specific combination of feature, value and impurity
+            # If it's better than the previous one, save the values
             for v in values:
                 imp = self.__try_split(x, y, f, v)
                 if imp > best_imp:
@@ -59,32 +66,45 @@ class MixedSplitter:
 
         return best_f, best_v, best_imp
 
+    # Try a specific split
+    # Parameters
+    #   x: x data
+    #   y: y data
+    #   f: feature
+    #   t: value
     def __try_split(self, x, y, f, t):
-        l_idx = x[:, f] <= t
-        r_idx = x[:, f] > t
+        left_idx = x[:, f] <= t
+        right_idx = x[:, f] > t
 
-        return self.__impurity_split(y, y[l_idx, :], y[r_idx, :])
+        return self.__impurity_split(y, y[left_idx, :], y[right_idx, :])
 
+    # Calculate the impurity of a node
     def __impurity_node(self, y):
+        # Calculate the impurity value for the classification task
         def impurity_class(y_class):
+            # FIXME: this is one of the bottlenecks
             y_class = y_class.astype(int)
             freq = np.bincount(y_class) / y_class.size
             freq = freq[freq != 0]
             return 0 - np.array([f * np.log2(f) for f in freq]).sum()
 
+        # Calculate the impurity value for the regression task
         def impurity_reg(y_reg):
             if np.unique(y_reg).size < 2:
                 return 0
 
             n_bins = 100
+            # FIXME: this is one the bottlenecks
             freq, _ = np.histogram(y, bins=n_bins, density=True)
             proba = (freq + 1) / (freq.sum() + n_bins)
             bin_width = (y_reg.max() - y_reg.min()) / n_bins
 
             return 0 - bin_width * (proba * np.log2(proba)).sum()
 
+        # TODO: what is this delta?
         delta = 0.0001
         imp = np.zeros(self.n_targets)
+        # Calculate the impurity value for each of the targets(classification or regression)
         for i in range(self.n_targets):
             if i in self.class_targets:
                 imp[i] = impurity_class(y[:, i]) + delta
@@ -92,6 +112,7 @@ class MixedSplitter:
                 imp[i] = impurity_reg(y[:, i]) + delta
         return imp
 
+    # Calculate the impurity of a split
     def __impurity_split(self, y, y_left, y_right):
         n_parent = y.shape[0]
         n_left = y_left.shape[0]
@@ -114,6 +135,12 @@ class MixedSplitter:
                 return gain.max()
 
 
+# Build a Random Tree
+# Parameters:
+#   max_features:
+#   min_samples_leaf: minimum amount of samples in each leaf
+#   choose_split: method used to find the best split
+#   class_targets: features that are part of the classification task
 class MixedRandomTree:
     def __init__(self,
                  max_features='sqrt',
@@ -228,6 +255,13 @@ class MixedRandomTree:
         print_l(0, 0)
 
 
+# Build the Random Forest model
+# Parameters:
+#   n_estimators: number of trees in the forest
+#   max_features:
+#   min_samples_leaf: minimum amount of samples in each leaf
+#   choose_split: method used to find the best split
+#   class_targets: features that are part of the classification task
 class MixedRandomForest:
     def __init__(self,
                  n_estimators=10,
@@ -252,15 +286,19 @@ class MixedRandomForest:
             y = y.reshape((y.size, 1))
         self.n_targets = y.shape[1]
 
+        # Get the classification labels
         for i in filter(lambda j: j in self.class_targets, range(self.n_targets)):
             self.class_labels[i] = np.unique(y[:, i])
 
         n_train = x.shape[0]
+        # Train the random trees that are part of the forest
         for i in range(self.n_estimators):
             m = MixedRandomTree(self.max_features,
                                 self.min_samples_leaf,
                                 self.choose_split,
                                 self.class_targets)
+
+            # It is a random forest so the trees are built with random subsets of the data
             sample_idx = np.random.choice(np.arange(n_train),
                                           n_train,
                                           replace=True)
@@ -277,8 +315,10 @@ class MixedRandomForest:
 
         pred_avg = np.zeros((n_test, self.n_targets))
         for i in range(self.n_targets):
+            # Predict categorical value
             if i in self.class_targets:
                 pred_avg[:, i], _ = scipy.stats.mode(pred[:, i, :].T)
+            # Predict numerical value
             else:
                 pred_avg[:, i] = pred[:, i, :].mean(axis=1)
 
@@ -335,7 +375,6 @@ def cross_validation(model,
                      class_eval=acc,
                      reg_eval=rmse,
                      verbose=False):
-
     class_targets = class_targets if class_targets else []
 
     idx = np.random.permutation(x.shape[0])

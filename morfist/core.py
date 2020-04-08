fix: incorrect results

morfist/core.py
@@ -7,7 +7,11 @@ from numba import njit
 # Calculate the impurity value for the classification task
 @njit
 def impurity_classification(y_classification):
-    frequency = np.bincount(y_classification.astype(np.int16)) / y_classification.size
+    y_class = y_classification.astype(np.int16)
+
+    # Calculate frequencies
+    frequency = np.bincount(y_class) / y_classification.size
+    frequency = frequency[frequency != 0]
 
     result = 0
     for f in frequency:
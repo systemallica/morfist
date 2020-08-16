a64d890f91df4bf99d7be9ecdc70a24a22ea212a
Andrés Reverón Molina <andressser@gmail.com>
2020-08-16 10:54:55 +0200

    modified mixed test for benchmarking

--- tests/test_mixed.py
+++ tests/test_mixed.py
@@ -3,8 +3,7 @@ from time import perf_counter
 import numpy as np
 from sklearn.datasets import load_boston, load_breast_cancer
 
-from morfist.algo.evaluation import cross_validation
-from morfist import MixedRandomForest
+from morfist import MixedRandomForest, MixedRandomForestLegacy, cross_validation
 
 # Configuration
 # Number of tress of the random forest
@@ -20,13 +19,13 @@ x_classification, y_classification = load_breast_cancer(return_X_y=True)
 def test_mix_1():
     # Create data with data transposed and stacked vertically, X is regression, Y is classification
     x_mix_1, y_mix_1 = x_regression, np.vstack([y_regression, y_regression < y_regression.mean()]).T
-    t1_start = perf_counter()
+    t_start = perf_counter()
 
     mix_rf = MixedRandomForest(
         n_estimators=n_trees,
         min_samples_leaf=1,
         classification_targets=[1],
-        choose_split='max'
+        choose_split='mean'
     )
 
     mix_scores = cross_validation(
@@ -36,9 +35,32 @@ def test_mix_1():
         folds=n_folds,
         classification_targets=[1]
     )
-    t1_stop = perf_counter()
-    time = t1_stop - t1_start
+    t_stop = perf_counter()
+    time = t_stop - t_start
     print("Elapsed time morfist mix:", time)
+    print('Mixed output: ')
+    print('\ttask 1 (original) (rmse):', mix_scores[0])
+    print('\ttask 2 (additional) (accuracy)', mix_scores[1])
+
+    t_start = perf_counter()
+
+    mix_rf_l = MixedRandomForestLegacy(
+        n_estimators=n_trees,
+        min_samples_leaf=1,
+        class_targets=[1],
+        choose_split='mean'
+    )
+
+    mix_scores = cross_validation(
+        mix_rf_l,
+        x_mix_1,
+        y_mix_1,
+        folds=n_folds,
+        classification_targets=[1]
+    )
+    t_stop = perf_counter()
+    time = t_stop - t_start
+    print("Elapsed time morfist legacy mix:", time)
     print('Mixed output: ')
     print('\ttask 1 (original) (rmse):', mix_scores[0])
     print('\ttask 2 (additional) (accuracy)', mix_scores[1])
@@ -54,7 +76,7 @@ def test_mix_2():
         n_estimators=n_trees,
         min_samples_leaf=1,
         classification_targets=[0],
-        choose_split='max'
+        choose_split='mean'
     )
 
     mix_scores = cross_validation(
@@ -67,6 +89,29 @@ def test_mix_2():
     t2_stop = perf_counter()
     time = t2_stop - t2_start
     print("Elapsed time morfist mix 2:", time)
+    print('Mixed output: ')
+    print('\ttask 1 (original) (accuracy):', mix_scores[0])
+    print('\ttask 2 (additional) (rmse):', mix_scores[1])
+
+    t_start = perf_counter()
+
+    mix_rf_l = MixedRandomForestLegacy(
+        n_estimators=n_trees,
+        min_samples_leaf=1,
+        class_targets=[1],
+        choose_split='mean'
+    )
+
+    mix_scores = cross_validation(
+        mix_rf_l,
+        x_mix_2,
+        y_mix_2,
+        folds=n_folds,
+        classification_targets=[1]
+    )
+    t_stop = perf_counter()
+    time = t_stop - t_start
+    print("Elapsed time morfist legacy mix 2:", time)
     print('Mixed output: ')
     print('\ttask 1 (original) (accuracy):', mix_scores[0])
     print('\ttask 2 (additional) (rmse):', mix_scores[1])
